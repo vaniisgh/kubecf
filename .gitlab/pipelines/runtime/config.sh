ac74abd708139026ffb3fad9cad225d6a87698ac
Dimitris Karakasilis <DKarakasilis@suse.com>
2019-11-21 12:43:37 +0200

    Settings to work with kind

--- .gitlab/pipelines/runtime/config.sh
+++ .gitlab/pipelines/runtime/config.sh
@@ -2,9 +2,9 @@
 
 export KUBECF_NAMESPACE="kubecf"
 export CF_OPERATOR_NAMESPACE="cfo"
-export CLUSTER_IP="10.43.0.255"
-export SYSTEM_DOMAIN="${CLUSTER_IP}.nip.io"
-: "${EIRINI_ENABLED:=false}"
+export CLUSTER_IP="10.96.0.255"
+export SYSTEM_DOMAIN="172.17.0.2.nip.io"
+: "${EIRINI_ENABLED:=true}"
 export EIRINI_ENABLED
 export KUBECF_CHART_TARGET="//deploy/helm/kubecf:chart"
 export BOSHDEPLOYMENT_CRD="boshdeployments.quarks.cloudfoundry.org"
